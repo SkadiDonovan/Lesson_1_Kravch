a83f435c97b8db1afc72a03257e4be12fec10832
Skadi <fant1maz@yandex.ru>
2022-07-09 17:59:01 +0300

    Небольшое исправление

--- AdditionalTusk/Program.cs
+++ AdditionalTusk/Program.cs
@@ -5,14 +5,12 @@
 
 Console.Write("Введите трехзначное число ");
 
-bool enterCorrectNumber = true;
 int number = 0;
 int lastNumber = 0;
 
-while (enterCorrectNumber){
+while (true){
     number = int.Parse(Console.ReadLine());
-    if (number > 99){
-        enterCorrectNumber = false;
+    if (number > 99 && number < 1000){
         break;
     } 
     Console.Write("Введенное число не трехзначное. Попробуйте еще раз ");
